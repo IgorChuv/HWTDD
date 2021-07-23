6ddd7567ed8f97d2a3a121bd22feb4c94dd6b629
Igor Chuvilov <igorchuv74@gmail.com>
2021-07-23 11:11:05 +0500

    Homeworks.Test Driven Development.Realization

--- HWTddDddCreditCalculator/src/main/java/CreditCalculator.java
+++ HWTddDddCreditCalculator/src/main/java/CreditCalculator.java
@@ -1,20 +1,27 @@
 public class CreditCalculator {
 
     //Рассчет аннуитетного платежа по кредиту
-    public static double calcOfMonthlyPayment() {
-        //TODO
+    public static double calcOfMonthlyPayment(int creditAmount, double interestRate, int numberOfMount) {
+        double interestRatePerMonthInPercent = interestRate / 100 / 12;
+        return (interestRatePerMonthInPercent
+                * Math.pow(1 + interestRatePerMonthInPercent, numberOfMount))
+                / (Math.pow(1 + interestRatePerMonthInPercent, numberOfMount) - 1)
+                * creditAmount;
     }
 
     //Рассчет общей суммы к возврату в банк
-    public static double calcTotalRefundAmount() {
-        //TODO
+    public static double calcTotalRefundAmount(int creditAmount, double interestRate, int numberOfMount) {
+        return calcOfMonthlyPayment(creditAmount, interestRate, numberOfMount) * numberOfMount;
     }
 
     //Рассчет переплаты за весь период
-    public static double calcOverpaymentForTheEntirePeriod() {
-        //TODO
+    public static double calcOverpaymentForTheEntirePeriod(int creditAmount, double interestRate, int numberOfMount) {
+        return calcTotalRefundAmount(creditAmount, interestRate, numberOfMount) - creditAmount;
     }
 
     public static void main(String[] args) {
+        System.out.printf("Ежемесячный платёж: %.2f руб.\n", calcOfMonthlyPayment(20000, 12, 36));
+        System.out.printf("Общая сумма возврата: %.2f руб.\n", calcTotalRefundAmount(20000, 12, 36));
+        System.out.printf("Переплата за весь период: %.2f руб.\n", calcOverpaymentForTheEntirePeriod(20000, 12, 36));
     }
 }
